5f2e4ede84a4bc9ed5f3a638abde8d906c23fbe0
Alberto Betanzos <albertobetpad17@gmail.com>
2022-10-24 21:23:48 +0200

    Segundo commit

--- examen.php
+++ examen.php
@@ -1,1 +1,8 @@
-<?php echo 'AlbertoBP a 24 en un dia fabuloso'; ?>
+<html>
+	<head>
+		<tittle>Examen potenjia de PHP</tittle>
+	</head>
+	<body>
+		<?php echo <h1>AlbertoBP</h1> a <h2>24</h2> <h3>en un dia fabuloso</h3>; ?>
+	</body
+</html>
